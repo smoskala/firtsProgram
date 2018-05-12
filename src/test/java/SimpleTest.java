be368a24a69ed37bd9d236609e086b140a3d1b63
smoskala <sylwestermoskala@gmail.com>
2018-05-12 23:21:57 +0200

    end2 commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by sylwe on 12.05.2018.
+ */
+public class SimpleTest {
+
+    @Test
+    public void test1() {
+        System.out.println("Test 1 works");
+    }
+}
